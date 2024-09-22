69f00d11158ac676b0c7d17705337dfc03414b6d
Vignesh-1512 <svignesh660@gmail.com>
2024-09-22 14:22:44 +0530

    29

--- src/Greedy Algo/01. N meeting in a Room.java
+++ src/Greedy Algo/01. N meeting in a Room.java
@@ -24,3 +24,81 @@
 //    N log N for sorting
 //Sc:- O(N)
 
+//{ Driver Code Starts
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+
+        while (t-- > 0) {
+            String inputLine[] = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(inputLine[0]);
+
+            int start[] = new int[n];
+            int end[] = new int[n];
+
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) start[i] = Integer.parseInt(inputLine[i]);
+
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) end[i] = Integer.parseInt(inputLine[i]);
+
+            int ans = new Solution().maxMeetings(n, start, end);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+class meeting{
+    int start;
+    int end;
+    int pos;
+    meeting(int start, int end, int pos){
+        this.start=start;
+        this.end=end;
+        this.pos=pos;
+    }
+}
+class meetingComparator implements Comparator<meeting>{
+    @Override
+    public int compare(meeting m1, meeting m2)
+    {
+        if(m1.end<m2.end)   return -1;
+        else if(m1.end>m2.end)  return 1;
+        else if(m1.pos<m2.pos)  return -1;
+        return 1;
+    }
+}
+class Solution {
+    // Function to find the maximum number of meetings that can
+    // be performed in a meeting room.
+    public int maxMeetings(int n, int start[], int end[]) {
+        // add your code here
+        ArrayList<meeting> meet = new ArrayList<>();
+        for(int i=0; i<n; i++)
+            meet.add(new meeting(start[i],end[i],i+1));
+
+        meetingComparator mc = new meetingComparator();
+        Collections.sort(meet,mc);
+
+        ArrayList<Integer>ans = new ArrayList<>();
+        ans.add(meet.get(0).pos);
+        int limit = meet.get(0).end;
+
+        for(int i=1; i<n; i++)
+        {
+            if(meet.get(i).start>limit)
+            {
+                limit=meet.get(i).end;
+                ans.add(meet.get(i).pos);
+            }
+        }
+        return ans.size();
+    }
+}
